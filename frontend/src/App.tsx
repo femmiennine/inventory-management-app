094611df7fd76ddb7c5daf687a9861e326ffac0a
Frances Fogarty <53113092+femmiennine@users.noreply.github.com>
2025-03-26 09:58:40 +0100

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-red-900 font-bold underline ml-8'>Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
